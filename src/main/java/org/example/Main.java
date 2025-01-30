b4f665842a351f210a4715659936034b0ce46ba4
Yogesh Shah <yogeshshah@Yogeshs-MacBook-Pro.local>
2025-01-30 10:47:09 +0530

    minor changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,13 @@ package org.example;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello world!");
-        System.out.println("new comment");
-        System.out.println("another comment");
-        System.out.println("new comment");
+        while (true) {
+            System.out.println("Hello world!");
+            try {
+                Thread.sleep(1000);  // Keeps the application running
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
