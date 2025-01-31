885a5116f0ce7c37e837cef4166f572cd1c7107c
Yogesh Shah <yogeshshah@Yogeshs-MacBook-Pro.local>
2025-01-31 11:39:00 +0530

    minor changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main{
     public static void main(String[] args){
         while (true) {
+            //new comment
             System.out.println("Hello world!");
             try {
                 Thread.sleep(1000);  // Keeps the application running
